1aebb353e2cdba42ec15b09f8d873e827be8c6a1
Filipp1232 <gorpenko.filipp@mail.ru>
2015-06-22 17:39:19 +0200

    first commit

---
+++ src/main/java/com/gmail/gorpenko/Main.java
@@ -0,0 +1,10 @@
+package com.gmail.gorpenko;
+
+/**
+ * Created by Filipp on 2015-06-21.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
