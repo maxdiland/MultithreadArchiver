ddbf31b8990ad9ebf72981627bcd77b76193e258
Max Diland <maxdiland@gmail.com>
2015-07-05 20:22:35 +0300

    iss04 Implement SimpleCompressor logic

--- src/main/java/com/gmail/gorpenko/decompressor/SimpleDecompressor.java
+++ src/main/java/com/gmail/gorpenko/decompressor/SimpleDecompressor.java
@@ -1,13 +1,35 @@
 package com.gmail.gorpenko.decompressor;
 
+import com.gmail.gorpenko.exception.CompressionDecompressionException;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.zip.GZIPInputStream;
 
 /**
  * author Maksim Diland
  */
 public class SimpleDecompressor implements Decompressor {
+
     @Override
     public void decompress(File fileToDecompress, File destinationFile) {
+        try ( InputStream inputStream = new BufferedInputStream(new GZIPInputStream(new FileInputStream(fileToDecompress)));
+              OutputStream outputStream = new BufferedOutputStream(new FileOutputStream(destinationFile)) ) {
 
+            int readByte;
+            while ( (readByte = inputStream.read()) != -1 ) {
+                outputStream.write(readByte);
+            }
+            outputStream.flush();
+
+        } catch (IOException e) {
+            throw new CompressionDecompressionException(e);
+        }
     }
 }
